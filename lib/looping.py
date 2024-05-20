9983706cb70980ef1c297d3f5a28db23fcaedc9b
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-20 18:52:32 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range (1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number) 
+            
+happy_new_year()
+fizzbuzz()                       
